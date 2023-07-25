e23ff1832773a00d85566a261531f8a5c068fb14
Sam Westmoreland <samwestmoreland@gmail.com>
2023-07-25 22:06:30 +0100

    t.Errorf doesn't support wrapping

--- src/position/position_test.go
+++ src/position/position_test.go
@@ -10,7 +10,7 @@ import (
 func TestGetPositionFromFEN(t *testing.T) {
 	fen, err := ParseFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
 	if err != nil {
-		t.Errorf("Error in ParseFEN: %w", err)
+		t.Errorf("Error in ParseFEN: %s", err)
 	}
 	pos := getPositionFromFEN(fen)
 	if pos == nil {
@@ -20,7 +20,7 @@ func TestGetPositionFromFEN(t *testing.T) {
 	// Check that there is a pawn on e4
 	e4, err := board.ParseSquare("e4")
 	if err != nil {
-		t.Errorf("Error in ParseSquare: %w", err)
+		t.Errorf("Error in ParseSquare: %s", err)
 	}
 	piece := pos.White[e4]
 	if piece == nil {
