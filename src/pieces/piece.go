7be0a35fffbb6eb790673902f63bdb85fc786bf8
Sam Westmoreland <samwestmoreland@gmail.com>
2023-07-25 21:19:39 +0100

    FromChar function

--- src/pieces/piece.go
+++ src/pieces/piece.go
@@ -25,3 +25,34 @@ type Piece interface {
 	GetCurrentSquare() board.Square
 	GetLegalMoves() []board.Square
 }
+
+func FromChar(char rune) Piece {
+	switch char {
+	case 'K':
+		return NewKing(board.White)
+	case 'k':
+		return NewKing(board.Black)
+	case 'Q':
+		return NewQueen(board.White)
+	case 'q':
+		return NewQueen(board.Black)
+	case 'R':
+		return NewRook(board.White)
+	case 'r':
+		return NewRook(board.Black)
+	case 'B':
+		return NewBishop(board.White)
+	case 'b':
+		return NewBishop(board.Black)
+	case 'N':
+		return NewKnight(board.White)
+	case 'n':
+		return NewKnight(board.Black)
+	case 'P':
+		return NewPawn(board.White)
+	case 'p':
+		return NewPawn(board.Black)
+	default:
+		return nil
+	}
+}
