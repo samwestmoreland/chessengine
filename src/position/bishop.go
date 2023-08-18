bfba36df004b85bbfb589eb4d0d5a1b611bdf337
Sam Westmoreland <samwestmoreland@gmail.com>
2023-08-18 22:27:17 +0100

    Only need to iterate over 7 squares max

--- src/position/bishop.go
+++ src/position/bishop.go
@@ -41,7 +41,7 @@ func (b *Bishop) GetMoves(pos *Position) ([]moves.Move, error) {
 	// until we hit a piece or the edge of the board
 	for _, direction := range []board.Direction{board.NorthEast, board.NorthWest, board.SouthEast, board.SouthWest} {
 		oldSquare := b.CurrentSquare
-		for i := 1; i < 8; i++ {
+		for i := 1; i < 7; i++ {
 			newSquare := oldSquare.Translate(direction)
 			if !newSquare.Valid() {
 				break
