1ef4c7332677473e67d8d3080f04b8c470afb55b
Sam Westmoreland <samwestmoreland@gmail.com>
2023-09-06 22:28:29 +0100

    Test getMaterialScore

--- src/eval/shannon_internal_test.go
+++ src/eval/shannon_internal_test.go
@@ -33,3 +33,24 @@ func TestClearlyWinningPositionForBlack(t *testing.T) {
 		t.Errorf("Expected score to be negative, got %v", score)
 	}
 }
+
+func TestMaterialScore(t *testing.T) {
+	expectedResults := map[string]int{
+		"r2q1rk1/ppp1bppp/3pp1n1/1B6/1n1PP2P/2N2PB1/P1P1QP2/R3K2R b KQ - 1 13": -1,
+		"r4rk1/4b1pp/n2pp1p1/qpp5/p2PP3/P1N1QPB1/B1P2P2/R3K2R w KQ - 0 21":     1,
+		"r7/6pk/n3B1pp/q3Pr2/p3N2R/p3QP2/2P1KP2/7R w - - 1 30":                 2,
+		"8/8/2BPnk2/b1P3p1/2K2pp1/6P1/5B1P/4q3 w - - 0 50":                     -8,
+		"8/8/1p6/6K1/7p/1P4k1/8/6b1 w - - 2 41":                                -4,
+	}
+
+	ev := NewShannonEvaluator()
+
+	for fen, expectedScore := range expectedResults {
+		parsed, _ := position.ParseFEN(fen)
+		pos := position.NewPositionFromFEN(parsed)
+		if score := ev.getMaterialScore(pos); score != expectedScore {
+			t.Errorf("Expected material score to be %v, got %v", expectedScore, score)
+		}
+	}
+
+}
