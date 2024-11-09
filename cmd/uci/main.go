cff880c86592621085ec3cedea041e1186362d98
Sam Westmoreland <samwestmoreland@gmail.com>
2024-11-09 16:46:17 +0000

    Refactor uci to have a pointer to an engine and a position

--- cmd/uci/main.go
+++ cmd/uci/main.go
@@ -6,72 +6,131 @@ import (
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/samwestmoreland/chessengine/internal/engine"
-	"github.com/samwestmoreland/chessengine/internal/tables"
+	"github.com/samwestmoreland/chessengine/internal/movegen"
+	"github.com/samwestmoreland/chessengine/internal/position"
 )
 
-type command struct {
-	name string
-	args []string
+type UCI struct {
+	engine   *engine.Engine
+	position *position.Position
+	writer   *bufio.Writer
+	reader   *bufio.Reader
 }
 
-var lookupTable tables.Lookup
-
-func main() {
-	if err := initialiseLookupTables(&lookupTable); err != nil {
-		log.Fatal(err)
+func NewUCI(writer *bufio.Writer, reader *bufio.Reader) (*UCI, error) {
+	if err := movegen.Initialise(); err != nil {
+		return nil, err
 	}
 
-	os.Exit(runEngine())
-}
-
-func runEngine() int {
 	eng, err := engine.NewEngine()
 	if err != nil {
-		return 1
+		return nil, err
 	}
 
-	writer := bufio.NewWriter(os.Stdout)
-	reader := bufio.NewReader(os.Stdin)
+	return &UCI{
+		engine: eng,
+		writer: writer,
+		reader: reader,
+	}, nil
+}
 
+func (u *UCI) Run() error {
 	for {
-		_, err := writer.WriteString("engine ready\n")
-		if err != nil {
-			panic(err)
+		if _, err := u.writer.WriteString("engine ready\n"); err != nil {
+			return err
 		}
+		u.writer.Flush()
 
-		writer.Flush()
-
-		cmd, err := reader.ReadString('\n')
+		cmdStr, err := u.reader.ReadString('\n')
 		if err != nil {
-			_, err := writer.WriteString("Error reading input\n")
-			if err != nil {
-				return 1
+			if _, err := u.writer.WriteString("Error reading input\n"); err != nil {
+				return err
 			}
-
-			writer.Flush()
-
+			u.writer.Flush()
 			continue
 		}
 
-		parsed := parseCmd(cmd)
-		resp, quit := handleCommand(parsed, eng)
+		cmd := parseCmd(cmdStr)
+		resp, quit := u.handleCommand(cmd)
 
-		_, err = resp.WriteTo(writer)
-		if err != nil {
-			return 1
+		if _, err := resp.WriteTo(u.writer); err != nil {
+			return err
 		}
-
-		writer.Flush()
+		u.writer.Flush()
 
 		if quit {
 			break
 		}
 	}
+	return nil
+}
 
-	return 0
+func (u *UCI) handleCommand(cmd *command) (*bytes.Buffer, bool) {
+	var resp bytes.Buffer
+	var quit bool
+
+	switch cmd.name {
+	case "uci":
+		resp.WriteString("id name Toto Chess Engine\n")
+		resp.WriteString("id author Sam Westmoreland\n")
+	case "quit", "exit", "bye", "q":
+		resp.WriteString("bye!\n")
+
+		quit = true
+	case "position":
+		u.handlePositionCmd(cmd, &resp)
+
+	case "isready":
+		resp.WriteString("readyok\n")
+	default:
+		resp.WriteString("unknown command\n")
+	}
+
+	return &resp, quit
+}
+
+func (u *UCI) handlePositionCmd(cmd *command, resp *bytes.Buffer) {
+	if len(cmd.args) == 0 {
+		resp.WriteString("too few arguments. expected `position <fen>` or `position startpos`\n")
+		return
+	}
+
+	if cmd.args[0] == "startpos" {
+		pos, err := position.NewPosition()
+		if err != nil {
+			panic(err)
+		}
+
+		u.position = pos
+		resp.WriteString("set up starting position\n")
+
+		pos.Print(os.Stdout)
+
+		return
+	}
+
+	// Handle FEN string case...
+}
+
+func main() {
+	uci, err := NewUCI(
+		bufio.NewWriter(os.Stdout),
+		bufio.NewReader(os.Stdin),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := uci.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type command struct {
+	name string
+	args []string
 }
 
 func parseCmd(cmd string) *command {
@@ -83,72 +142,4 @@ func parseCmd(cmd string) *command {
 		name: parts[0],
 		args: parts[1:],
 	}
-}
-
-func handleCommand(cmd *command, eng *engine.Engine) (*bytes.Buffer, bool) {
-	var resp bytes.Buffer
-
-	var quit bool
-
-	// Process command
-	switch cmd.name {
-	case "uci":
-		mustWrite(&resp, "id name Toto Chess Engine\n")
-		mustWrite(&resp, "id author Sam Westmoreland\n")
-	case "quit", "exit", "bye", "q":
-		mustWrite(&resp, "bye!\n")
-
-		quit = true
-	case "position":
-		posResp := handlePositionCmd(cmd, eng)
-
-		_, err := resp.Write(posResp.Bytes())
-		if err != nil {
-			panic(err)
-		}
-	case "isready":
-		mustWrite(&resp, "readyok\n")
-	default:
-		mustWrite(&resp, "unknown command\n")
-	}
-
-	return &resp, quit
-}
-
-func handlePositionCmd(cmd *command, eng *engine.Engine) *bytes.Buffer {
-	var resp bytes.Buffer
-
-	if len(cmd.args) == 0 || cmd.args == nil {
-		mustWrite(&resp, "too few arguments. expected `position <fen>` or `position startpos`\n")
-
-		return &resp
-	}
-
-	if cmd.args[0] == "startpos" {
-		mustWrite(&resp, "set up starting position\n")
-
-		return &resp
-	}
-
-	return &resp
-}
-
-func mustWrite(buf *bytes.Buffer, s string) {
-	_, err := buf.WriteString(s)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func initialiseLookupTables(lookup *tables.Lookup) error {
-	log.Print("initialising lookup tables")
-	start := time.Now()
-
-	if err := tables.InitialiseLookupTables(lookup); err != nil {
-		return err
-	}
-
-	log.Printf("initialised lookup tables in %s", time.Since(start))
-
-	return nil
 }
